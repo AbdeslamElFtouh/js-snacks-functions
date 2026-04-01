d0a39ac5c5b297535d441d36317902f6840d9214
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-04-01 17:34:24 +0200

    ADD snack 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,24 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array 
+   contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function filter_by_initial(array, letter) {
+    let risultati = [];
+    
+    for (let i = 0; i < array.length; i++) {
+        let current_name = array[i];  
+        if (current_name.startsWith(letter)) {
+            risultati.push(current_name);
+        }
+    }
+    return risultati;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(filter_by_initial(names, "A"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
