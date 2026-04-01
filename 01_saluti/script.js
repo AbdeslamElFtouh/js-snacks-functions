0baaaeb8082eb74e499ea2e8ec97e3e604db613e
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-04-01 17:10:32 +0200

    ADD snack 1

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,12 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
-
+function saluti(name) {
+    let saluto_utente = 'Ciao ' + name;
+    return saluto_utente;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(saluti(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
