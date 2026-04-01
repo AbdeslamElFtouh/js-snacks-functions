26aca15736dd2e8d3f5280a930e7559b76d502f7
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-04-01 17:42:05 +0200

    ADD snack 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,11 +2,24 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
-
+function conta_vocali(stringa) {
+    let vocali = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    
+    for (let i = 0; i < stringa.length; i++) {
+        let current_char = stringa[i].toLowerCase();
+        
+        if (vocali.includes(current_char)) {
+            count++;
+        }
+    }
+    return count;
+}   
 
 // Invoca la funzione qui e stampa il risultato in console
+
+console.log(conta_vocali(word));
 
 
 
