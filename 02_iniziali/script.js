34ec4645b3cff5d06d707bc4a3d2ab544989fb9f
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-04-01 17:17:21 +0200

    ADD snack 2

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,21 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le 
+   iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function get_first_letters(arr) {
+    let first_letters_array = [];
+    for (let i = 0; i < arr.length; i++) {
+        let name = arr[i];
+        let first_letter = name.charAt(0);
+        first_letters_array.push(first_letter);
+    }
+    return first_letters_array;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(get_first_letters(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
